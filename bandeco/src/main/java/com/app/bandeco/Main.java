7373f02b3ae336a9425016027f46df5e2fb65fa2
Erick Pires <pires@erickpires.com>
2015-09-22 17:04:34 -0300

    Updated to AndroidStudio 1.3

--- bandeco/src/main/java/com/app/bandeco/Main.java
+++ bandeco/src/main/java/com/app/bandeco/Main.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
